cce94f94c846c79b434ac4faa4133628f17589d2
Wenlong Zhang <longlong198@gmail.com>
2023-03-09 13:53:54 -0500

    add board

---
+++ src/components/Board.jsx
@@ -0,0 +1,23 @@
+import './style.css';
+
+function Board() {
+  return (
+    <div>
+      <div className='row'>
+        <div className='square'>1</div>
+        <div className='square'>2</div>
+        <div className='square'>3</div>
+      </div>
+      <div className='row'>
+        <div className='square'>4</div>
+        <div className='square'>5</div>
+        <div className='square'>6</div>
+      </div>
+      <div className='row'>
+        <div className='square'>7</div>
+        <div className='square'>8</div>
+        <div className='square'>9</div>
+      </div>            
+    </div>
+  )
+}
