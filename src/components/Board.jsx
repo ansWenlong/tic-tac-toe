ea8c784ddb4ab19e110139534e718a9526028441
wenzhang <wenlong.zhang@ansys.com>
2023-03-10 10:22:52 -0500

    separate square into a component, add state to it

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,41 @@
+import { useState } from 'react';
 import './style.css';
+
+function Square () {
+  const [value, setValue] = useState(null);
+
+  const handleClick = () => {
+    setValue('X');
+    console.log(`clicked! ${value}`);
+  }
+
+  return (
+    <button 
+      className='Square'
+      onClick = {handleClick}
+    >
+      {value}
+    </button>
+  );
+}
 
 function Board() {
   return (
     <div>
       <div className='row'>
-        <div className='Square'>1</div>
-        <div className='Square'>2</div>
-        <div className='Square'>3</div>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='row'>
-        <div className='Square'>4</div>
-        <div className='Square'>5</div>
-        <div className='Square'>6</div>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='row'>
-        <div className='Square'>7</div>
-        <div className='Square'>8</div>
-        <div className='Square'>9</div>
+        <Square />
+        <Square />
+        <Square />     
       </div>            
     </div>
   )
